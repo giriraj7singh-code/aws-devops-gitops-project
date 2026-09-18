9c9f683fab0bdffc3938dbc0e70d3cbbd0793044
Giriraj Singh <giriraj7singh@gmail.com>
2026-09-18 17:26:10 +0000

    Add Nginx reverse proxy and Docker Compose

--- terraform/security.tf
+++ terraform/security.tf
@@ -26,3 +26,12 @@ resource "aws_vpc_security_group_egress_rule" "outbound" {
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
 }
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.devops.id
+  description       = "Public HTTP access through Nginx"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
